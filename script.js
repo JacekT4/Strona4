b80fc66225238c85bffc287f3a10be1e3e846c68
JacekT4 <jacekturski4@gmail.com>
2020-08-29 00:03:54 +0200

    druga wersja

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //skrypt js
+var nazwa1 = document.getElementById("produkt").value;
+var lista = document.getElementById("L");
+
+function dodajDoListy()
+{
+  document.getElementById("lista").innerHTML = nazwa1;
+}
