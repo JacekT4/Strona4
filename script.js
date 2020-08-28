4f2165c9d12d2dd0f84cdd3979c3e78fd3ce9784
JacekT4 <jacekturski4@gmail.com>
2020-08-29 01:31:40 +0200

    wersja trzecia

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-//skrypt js
-var nazwa1 = document.getElementById("produkt").value;
-var lista = document.getElementById("L");
+(function() {
+	var debug = false;                                                                     //narzedzie deweloperskie, po wykonanej pracy mozna zmienic true na false
+	var usunZListy = function usunZListy(wiersz) {
+		debug && console.log(wiersz);
+		wiersz.remove();
+	}	
+	var dodajDoListy = function dodajDoListy(lista, wartosc) {
+		debug && console.log(lista, wartosc);
+		var  wiersz = document.createElement("li");                      //var - deklarowanie w danym bloku, lepiej let a najlepiej const
+		var  usuwacz = document.createElement("button");
+		wiersz.innerHTML = wartosc;
+		usuwacz.innerHTML = "Usuń";
+		usuwacz.addEventListener("click", function() {
+			usunZListy(wiersz);		
+		});
+		wiersz.appendChild(usuwacz);
+		lista.appendChild(wiersz);
+	}
+	var dodaj = document.getElementById("dodaj");
+	var lista = document.getElementById("lista");
 
-function dodajDoListy()
-{
-  document.getElementById("lista").innerHTML = nazwa1;
-}
+    dodaj.addEventListener("click", function() {
+		var wartosc = document.getElementById("produkt").value;
+		dodajDoListy(lista, wartosc);
+	});
+})();
